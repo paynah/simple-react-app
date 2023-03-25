c96e11de7a922a757061e8d5cfda95e988a505c7
Nancy Yu <nancyyu09@gmail.com>
2023-03-25 11:01:19 -0700

    added button with counter label to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import './App.css';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+  
+  const onButtonClick = () => {
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-        Hello
+        <button onClick={onButtonClick}>Click Me</button>
+      <div id="counter-div">The button has been clicked {counter} times.</div>
     </div>
   );
 }
