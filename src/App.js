080180d62b9d7bfe8b89b7d0ab29b8986c374313
Nancy Yu <nancyyu09@gmail.com>
2023-03-25 11:31:51 -0700

    added TestButton component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import './App.css';
+import TestButton from "./TestButton";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  
+
   const onButtonClick = () => {
     setCounter(counter + 1);
   }
 
   return (
     <div className="App">
-        <button onClick={onButtonClick}>Click Me</button>
+      <TestButton onButtonClick={onButtonClick} />
       <div id="counter-div">The button has been clicked {counter} times.</div>
     </div>
   );
---
+++ src/TestButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function TestButton(props) {
+
+  return (
+    <button onClick={props.onButtonClick}>Click Me</button>
+  );
+}
